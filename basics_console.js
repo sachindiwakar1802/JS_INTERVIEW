f43476e086ccc4ade4cabb8d5b0d4a51b7014c18
sachin diwakar <sachindiwakar1802@gmail.com>
2026-05-16 16:23:58 +0530

    first commit

---
+++ basics_console.js
@@ -0,0 +1,43 @@
+// // 01 Log ypur name and favorite hobby to the console
+// console.log("sachin and my favorite hobby is codinga nd gym");
+// // 02 perform the operation and log the rsult 45*2-10;
+// let a = 45;
+// let b = 2;
+// let c = 10;
+// let result = a*2-10;
+// console.log(result);
+//03 use console.log() to show the current yeaer
+// console.log(new Date().getFullYear());
+// //04create two variable first and last name and concate them 
+// let first_name = "sachin";
+// let last_name = "diwakar";
+// let full_name = first_name+ " " +last_name
+// console.log(full_name);
+// //05 Trackig the value of variable by logging it before and after updating 
+// let p = 9;
+// console.log(p);
+// p = 8;
+// console.log(p)
+// //06 use console.erroe to show teh error msg in the console
+// console.error("this progrma is many error")
+// // diff btw the console.log() and the console.errr()
+
+// //07 log the sqr of the number 12 to the console
+// let n = 12;
+// let sqr = n*n;
+// console.log(sqr);
+// //08 print the type of variable holding the value 
+// let s = "sb.kdfshkv";
+// let type = typeof(s)
+// console.log(type)
+// //09 age , greater then 18 or not by useing teh if 
+// let age = 22;
+// if(age>=18){
+//   console.log("greater then 18")
+// }
+// else{
+//   console.log("you are younger then 18")
+// }
+// // 10 log the result 100/0 and observe the result 
+// console.log(100/0);
+
