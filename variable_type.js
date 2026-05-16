72092dd5190b512125e672ac99a399c8a4f6d3c9
sachin diwakar <sachindiwakar1802@gmail.com>
2026-05-16 16:57:57 +0530

    cod

---
+++ variable_type.js
@@ -0,0 +1,18 @@
+//**variable and data types quetions**//
+// 11 declare a variable using let and log its value
+let m = 12;
+console.log(m)
+//12 create a consonant to store the value of pi and log it 
+const PI = 22/7;
+console.log(PI)
+//12 ckeck the type of null 
+console.log(typeof(null))
+//13 Reassign a value to a variable declared with let and log the result 
+let val = 12;
+console.log(val);
+val = 34;
+console.log(val);
+//14 create a variable with a number as a string (eg "25") then log it 
+
+let value = "25";
+console.log(typeof(value))
