2191dc27ab82d1c74533c62caee24e75ca75c388
sachin diwakar <sachindiwakar1802@gmail.com>
2026-05-16 18:55:32 +0530

    code

---
+++ loops.js
@@ -0,0 +1,85 @@
+// //21 write a for loop to print numbers from 1 to 50
+
+// for(let i = 1;i<=50;i++){
+//   console.log(i)
+// }
+
+// //22 use a while loop to sum 1 to 10;
+// let n = 1;
+// let sum = 0;
+
+// while(n <= 10){
+//   sum = sum + n;
+//   n++;
+// }
+
+// console.log(sum);
+
+//23 
+
+// 24 write the loop that skips even numbers btw 1 to 20
+
+// for(let i=1;i<=20;i++){
+//   if(i%2!==0){
+//     console.log(i)
+//   }
+// }
+
+//25 use do while loop to log numbers from 5 to 1
+// n=5;
+// do{
+//   console.log(n)
+//   --n
+// }
+// while(n>=1)
+
+// //26 for loop that calculate the factorial of teh 5 
+// let n = 5;
+// let f = 1;
+
+// for(let i = 1; i <= n; i++){
+//    f = f * i;
+// }
+
+// console.log(f);
+
+// // 27 print the grid 3x3
+// for(let i=0;i<3;i++){
+//   let row = ""
+//   for(let j =0;j<3;j++){
+//     row = row + "x"
+     
+//   }
+//  console.log(row)
+// }
+
+//use a for loop to revese  array
+
+// let arr = [1,3,4,5,7];
+// let newarr  = [];
+// for(let i=arr.length-1;i>=0;i--){
+//   newarr.push(arr[i])
+// } 
+// console.log(newarr)
+
+// //29 write a while loop that logs numbers from 1 to 100 divisible by 5
+// let n = 1;
+// while(n<=100){
+//   if(n%5===0){
+//     console.log(n)
+//   }
+//   n++
+// }
+
+// 30 use for in loop iterate over an object and log its keys
+
+// let person = {
+//   name:"sachin",
+//   nme:"sachin",
+//   nam:"sachin",
+// }
+// for(key in person){
+//   console.log(key,person[key]
+//   )
+// }
+
